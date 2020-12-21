d14caacd0dda3853ca55f4802561f5b513b72dec
Annegret <annegret.junker@gmx.de>
2020-12-21 17:05:08 +0100

    Initial commit

---
+++ src/test/java/com/allianz/sdw/poc/configclient/ConfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianz.sdw.poc.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
